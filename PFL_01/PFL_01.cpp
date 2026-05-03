223581ab5db39bad1fe8256f028304da34ef6e41
frostdawn <nicozhong@gmail.com>
2026-05-03 22:02:55 +0800

    Env & Github

--- PFL_01/PFL_01.cpp
+++ PFL_01/PFL_01.cpp
@@ -1,9 +1,11 @@
+// Hello World! of CPP
+// #include <iostream>
+// #include <ostream>
+//
+// int main(int argc, char* argv[])
+// {
+//     std::cout << "Hello World!" << std::endl;
+//     return 0;
+// }
 
-#include <iostream>
-#include <ostream>
-
-int main(int argc, char* argv[])
-{
-    std::cout << "Hello World!" << std::endl;
-    return 0;
-}
+// C++常见的面试题类型
